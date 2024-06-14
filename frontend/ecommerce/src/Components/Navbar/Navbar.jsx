9e1b836f6829640f8161c8b194e7bbda9782e747
anoushka <vyasanoushka@gmail.com>
2024-06-14 15:03:11 +0530

    first commit

---
+++ frontend/ecommerce/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>Shopper</p>
+      </div>
+      <ul className='nav-menu'>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+
+      <div className='nav-login-cart'>
+        <button>login</button>
+        <img src={cart_icon} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
